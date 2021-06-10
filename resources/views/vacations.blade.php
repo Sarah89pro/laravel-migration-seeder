71849c365739a5062ec70aa8b552d2bd54dd6189
Sarah89Pro <sarahproietti1@gmail.com>
2021-06-10 23:48:05 +0200

    .

--- resources/views/vacations.blade.php
+++ resources/views/vacations.blade.php
@@ -6,14 +6,14 @@
     <!--Tutti le vacations da DB-->
     @foreach ($vacations as $vacation)
     <article>
-        <h2>{{ $vacation->city }}</h2>
-        <div>{{ $vacation->accomodation }}</div>
-        <div>{{ $vacation->people }}</div>
-        <div>{{ $vacation->flight }}</div>
-        <div>{{ $vacation->description }}</div>
-        <div>{{ $vacation->check_in }}</div>
-        <div>{{ $vacation->check_out }}</div>
-        <div>{{ number_format($vacation->price, 0, ',', '.')}} €</div>
+        <h2>Città: {{ $vacation->city }}</h2>
+        <div>Sistemazione: {{ $vacation->accomodation }}</div>
+        <div>Numero di persone: {{ $vacation->people }}</div>
+        <div>Volo numero: {{ $vacation->flight }}</div>
+        <div>Descrizione {{ $vacation->description }}</div>
+        <div>Check-in: {{ $vacation->check_in }}</div>
+        <div>Check-out: {{ $vacation->check_out }}</div>
+        <div>Prezzo: {{ number_format($vacation->price, 0, ',', '.')}} €</div>
 
         @if (! $loop->last)
             <hr>
